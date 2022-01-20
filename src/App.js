10f62640916cef315ac0f24a08bb41660e6d162e
tar04 <taras200195@gmail.com>
2022-01-20 14:03:47 +0200

    added cw2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
 import Users from "./components/Users/Users";
+import {useState} from "react";
 
 function App() {
+
+    let [form, setForm] = useState({name: '', username: '', email: ''});
+
+    const send = (e) => {
+        e.preventDefault();
+        console.log(form);
+    }
+
+    const onChange = (e) => {
+        setForm({...form, [e.target.name]: e.target.value});
+    }
+
     return (
         <div>
             <div>
-                <label>Name: <input type="text"/></label>
-                <label>Username: <input type="text"/></label>
-                <label>Email: <input type="text"/></label>
-                <button>Find</button>
+                <form onSubmit={send}>
+                    <label>Name: <input type="text" name={'name'} value={form.name} onChange={onChange}/></label>
+                    <label>Username: <input type="text" name={'username'} value={form.username} onChange={onChange}/></label>
+                    <label>Email: <input type="text" name={'email'} value={form.email} onChange={onChange}/></label>
+                    <button>Find</button>
+                </form>
             </div>
             <div>
                 <Users/>
