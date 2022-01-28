4bf22aca96d70eb5333037b230e1697fde7626dd
tar04 <taras200195@gmail.com>
2022-01-28 20:06:42 +0200

    added сw4

--- src/App.js
+++ src/App.js
@@ -3,36 +3,40 @@ import {useReducer} from "react";
 import './App.css';
 
 const reducer = (state, action) => {
-   /* switch (action.type) {
+    switch (action.type) {
         case 'addCats':
-            return {...state, cats: state.cats.push(document.forms.catsInput.value)}
-    }*/
+            return {...state, cats: state.cats.push(document.forms.catsForm.elements.catsInput.value)}
+    }
 
 }
 
 function App() {
 
-    const [state, dispatch] = useReducer(reducer, {count1: 0, count2: 0});
+    const [state, dispatch] = useReducer(reducer, {cats: [], dogs: []});
+
+    const send = () => {
+        dispatch({type: 'addCats'})
+    }
 
     return (
         <div>
             <div className={'forms'}>
                 <div className={'form'}>
-                    <form><label>Add cat:<input type="text" name={'catsInput'}/></label>
-                        <button onClick={dispatch({type: 'addCats'})}>Save</button>
+                    <form name={'catsForm'}><label>Add cat:<input type="text" name={'catsInput'}/></label>
+                        <button>Save</button>
                     </form>
                 </div>
                 <div className={'form'}>
                     <form><label>Add dog:<input type="text" name={'addDogs'}/></label>
-                        <button onClick={dispatch({type: 'addDogs'})}>Save</button>
+                        <button>Save</button>
                     </form>
 
                 </div>
             </div>
             <hr/>
             <div className={'animals'}>
-               <div className={'cats'}>{state.count1}</div>
-                <div className={'dogs'}>{state.count2}</div>
+                {state.cats && (<div className={'cats'}>{state.cats}</div>)}
+                {state.dogs && (<div className={'dogs'}>{state.dogs}</div>)}
             </div>
         </div>
     );
