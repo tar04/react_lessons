ea5aa3058d61611c1cf06acae267cf3f5fefbdd5
tar04 <taras200195@gmail.com>
2022-02-05 17:39:55 +0200

    added hw6

---
+++ hw6.ts
@@ -0,0 +1,27 @@
+import {IUser} from "./interface/userInterface";
+
+
+const user:IUser = {
+    name:"Max",
+    age:18,
+    gender:'male'
+}
+
+//console.log(user)
+
+function sum(a:number,b:number):number{
+    return a+b;
+}
+function showSum(a:number,b:number){
+    console.log(a + b);
+}
+
+function incAge(someUser:IUser, inc:number):IUser{
+    someUser.age+=inc;
+    return someUser;
+}
+
+console.log(sum(1, 2));
+showSum(2,3);
+incAge(user, 2);
+//console.log(user)
