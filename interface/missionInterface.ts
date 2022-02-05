34f14da976f6c8ab0f793e3a56bdfe3c5e3aeda6
tar04 <taras200195@gmail.com>
2022-02-05 17:50:56 +0200

    added hw6

--- interface/missionInterface.ts
+++ interface/missionInterface.ts
@@ -1,34 +1,46 @@
 export interface IMission {
     mission_name: string;
     launch_date_local: string;
-    launch_site: {
-        site_name_long: string;
-    };
-    links: {
-        article_link: null|string;
-        video_link: string;
-    };
-    rocket: {
-        rocket_name: string;
-        first_stage: {
-            cores: [
-                {
-                    flight: number;
-                    core: {
-                        reuse_count: number;
-                        status: string;
-                    }
-                }
-            ]
-        };
-        second_stage: {
-            payloads: [
-                {
-                    payload_type: string;
-                    payload_mass_kg: number;
-                    payload_mass_lbs: number;
-                }
-            ]
-        }
-    }
+    launch_site: ILaunchSite;
+    links: ILinks;
+    rocket: IRocket
+}
+
+interface ILaunchSite {
+    site_name_long: string;
+}
+
+interface ILinks {
+    article_link: null | string;
+    video_link: string;
+}
+
+interface IRocket {
+    rocket_name: string;
+    first_stage: IFirstStage;
+    second_stage: ISecondStage;
+}
+
+interface IFirstStage {
+    cores: [ICores]
+}
+
+interface ICores {
+    flight: number;
+    core: ICore;
+}
+
+interface ICore{
+    reuse_count: number;
+    status: string;
+}
+
+interface ISecondStage{
+    payloads: [IPayloads];
+}
+
+interface IPayloads{
+    payload_type: string;
+    payload_mass_kg: number;
+    payload_mass_lbs: number;
 }
